e9a17e1580f0ceb8778c54ca5eb376bbe7d94272
MayankBhakat <subham.bhakat01@gmail.com>
2023-06-21 11:43:08 +0530

    final destination

---
+++ client/src/components/DefaultLayout.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+
+function DefaultLayout({children}) {
+ const {user} = useSelector(state => state.user)
+ 
+ const navigate = useNavigate()
+  return (
+    
+     <div className="main">
+         <div className="header flex justify-between p-5 shadow items-center">
+             <h1 className='text-3xl ml-7 font-bold cursor-pointer' onClick={()=>{
+                    navigate('/')
+             }}><b className='text-primary'>CADENCE</b> <b className='text-secondary'>CORNER</b></h1>
+             <div className='flex items-center gap-2 '>
+             {user.isAdmin?(
+                <h1 className='text-xl text-primary cursor-pointer'  onClick={()=>{
+                    navigate('/admin')
+                }}>ADMIN</h1>
+               
+             ):
+             <h1 className='text-xl'></h1>
+             }
+                <h1 className='text-xl'>{user?.name.toUpperCase()}</h1>
+                <i className="ri-logout-circle-r-line text-xl cursor-pointer" onClick={()=>{
+                    localStorage.removeItem('token')
+                    window.location.reload()
+                }}></i>
+             </div>
+         </div>
+         <div className="content m-10">
+             {children}
+         </div>
+     </div>
+  )
+}
+
+export default DefaultLayout
